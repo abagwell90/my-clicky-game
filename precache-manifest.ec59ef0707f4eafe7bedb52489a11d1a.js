40e86302ff537838eb97e5696a76a4f2a564c117
Andrew Bagwell <abagwell90@gmail.com>
2019-03-30 12:19:55 -0400

    Updates

---
+++ precache-manifest.ec59ef0707f4eafe7bedb52489a11d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "821599d0010d9adefbf41aaeeb9bc626",
+    "url": "/my-clicky-game/static/media/logo1-01.821599d0.png"
+  },
+  {
+    "revision": "df00b2044d40ff0a4324",
+    "url": "/my-clicky-game/static/js/runtime~main.afc3c0f5.js"
+  },
+  {
+    "revision": "03bbf86ed14de5fb6882",
+    "url": "/my-clicky-game/static/js/main.e40aa938.chunk.js"
+  },
+  {
+    "revision": "6931bf47837e578b0778",
+    "url": "/my-clicky-game/static/js/2.408f6dd2.chunk.js"
+  },
+  {
+    "revision": "03bbf86ed14de5fb6882",
+    "url": "/my-clicky-game/static/css/main.c1987bdf.chunk.css"
+  },
+  {
+    "revision": "6931bf47837e578b0778",
+    "url": "/my-clicky-game/static/css/2.1dd8720c.chunk.css"
+  },
+  {
+    "revision": "722b0559313edbf09d84b7015f24c0ec",
+    "url": "/my-clicky-game/index.html"
+  }
+];
